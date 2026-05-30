159a2d9d841ce08f652b76259760ffa09eddd3e3
technova232-code <technova232@gmail.com>
2026-05-30 19:21:12 +0000

    Rewrite settings and tip widget

--- battery_vault/lib/widgets/tip_card_widget.dart
+++ battery_vault/lib/widgets/tip_card_widget.dart
@@ -1,33 +1,16 @@
-import 'package:google_fonts/google_fonts.dart';
 import 'package:flutter/material.dart';
 
 class TipCardWidget extends StatelessWidget {
   final int index;
   final String tip;
-
   const TipCardWidget({super.key, required this.index, required this.tip});
-
-  static const _icons = [
-    Icons.brightness_low_outlined,
-    Icons.bluetooth_disabled_outlined,
-    Icons.apps_outlined,
-    Icons.battery_saver_outlined,
-    Icons.thermostat_outlined,
-  ];
-
-  static const _colors = [
-    Color(0xFF39FF14),
-    Color(0xFF00B4D8),
-    Color(0xFFFFD700),
-    Color(0xFF39FF14),
-    Color(0xFFFF6B6B),
-  ];
 
   @override
   Widget build(BuildContext context) {
-    final color = _colors[index % _colors.length];
-    final icon = _icons[index % _icons.length];
-
+    const colors = [Color(0xFF39FF14), Color(0xFF00B4D8), Color(0xFFFFD700), Color(0xFF39FF14), Color(0xFFFF6B6B)];
+    const icons = [Icons.brightness_low_outlined, Icons.bluetooth_disabled_outlined, Icons.apps_outlined, Icons.battery_saver_outlined, Icons.thermostat_outlined];
+    final color = colors[index % colors.length];
+    final icon = icons[index % icons.length];
     return Container(
       margin: const EdgeInsets.only(bottom: 10),
       padding: const EdgeInsets.symmetric(horizontal: 16, vertical: 14),
@@ -39,26 +22,12 @@ class TipCardWidget extends StatelessWidget {
       child: Row(
         children: [
           Container(
-            width: 36,
-            height: 36,
-            decoration: BoxDecoration(
-              color: color.withOpacity(0.1),
-              borderRadius: BorderRadius.circular(10),
-            ),
+            width: 36, height: 36,
+            decoration: BoxDecoration(color: color.withOpacity(0.1), borderRadius: BorderRadius.circular(10)),
             child: Icon(icon, color: color, size: 20),
           ),
           const SizedBox(width: 14),
-          Expanded(
-            child: Text(
-              tip,
-              style: const TextStyle(
-                color: Colors.white70,
-                ,
-                fontSize: 13,
-                height: 1.5,
-              ),
-            ),
-          ),
+          Expanded(child: Text(tip, style: TextStyle(color: Colors.white70, fontFamily: 'Cairo', fontSize: 13, height: 1.5))),
         ],
       ),
     );
